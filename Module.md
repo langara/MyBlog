429d745dc37a7a52780771f86a601f7542c615ac
Marek Langiewicz <marek.langiewicz@gmail.com>
2017-05-18 18:18:00 +0200

    Add initial gradle configuration and small working sample

---
+++ Module.md
@@ -0,0 +1,12 @@
+# Module MyBlog
+
+BLABLA.
+- fjdskal 
+- fdsjakfl
+- fja fdksa fjdksla fdjsa
+- fjdsakfa fdsafds
+
+# Package Kotlin Coroutines Intro in "TDD"
+
+This is my attempt to learn about kotlin coroutines and write some tests on the way.
+
---
+++ src/main/kotlin/pl/mareklangiewicz/myblog/CoroutinesSample.kt
@@ -0,0 +1,32 @@
+package pl.mareklangiewicz.myblog
+
+import org.junit.Test
+
+/**
+ * This is my attempt to learn about kotlin coroutines and write some tests on the way.
+ */
+class CoroutinesSample {
+
+    /**
+     * # H1
+     * something under H1
+     * ## H2
+     * something under H2
+     * ### H3
+     * something under H3
+     *
+     * And some list:
+     * - fjdksl
+     * - fjdskl fdks
+     * - fj sfkd fsjl
+     * And now the sample itself:
+     * @sample pl.mareklangiewicz.myblog.CoroutinesSample.bla
+     */
+    @Test
+    fun bla() {
+        println("bla")
+        println("bla bla")
+        println("bla bla bla")
+    }
+
+}
