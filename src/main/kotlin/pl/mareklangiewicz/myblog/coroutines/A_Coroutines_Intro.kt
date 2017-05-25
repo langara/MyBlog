bca6cfbd3dd5a606581eba6469e6fafd62d9194f
Marek Langiewicz <marek.langiewicz@gmail.com>
2017-05-25 14:17:52 +0200

    J under the hood 3 - separate error case

--- src/main/kotlin/pl/mareklangiewicz/myblog/coroutines/A_Coroutines_Intro.kt
+++ src/main/kotlin/pl/mareklangiewicz/myblog/coroutines/A_Coroutines_Intro.kt
@@ -304,6 +304,41 @@ class A_Coroutines_Intro {
     }
 
     /**
+     * Under the hood - error case investigation
+     * FIXME NOW: this example should never print "coroutine end", but it does immediately!
+     *
+     * @sample pl.mareklangiewicz.myblog.coroutines.A_Coroutines_Intro.J_underTheHood_3
+     */
+    @Test
+    fun J_underTheHood_3() {
+
+        val mynever: suspend () -> Unit = {
+            suspendCoroutine<Unit> { continuation ->
+                "Got continuation: $continuation, but I will never call .resume(Unit)".p
+            }
+        }
+
+        val coroutine: suspend () -> Unit = {
+            "coroutine start".p
+            mynever()
+            "coroutine end - THIS LINE SHOULD NEVER BE CALLED!".p
+        }
+
+        val completion = object : Continuation<Unit> {
+            override val context = EmptyCoroutineContext
+            override fun resume(value: Unit) { "completion: completed normally".p }
+            override fun resumeWithException(exception: Throwable) { "completion: exception: $exception".p }
+        }
+
+        "main: start".p
+        val continuation = coroutine.createCoroutine(completion)
+        "main: after createCoroutine".p
+        continuation.resume(Unit)
+        "main: after continuation resume (to start coroutine)".p
+        Thread.sleep(3000)
+        "main: after sleep 3000".p
+    }
+    /**
      * Sequence builder test
      *
      * @sample pl.mareklangiewicz.myblog.coroutines.A_Coroutines_Intro.K_sequence
