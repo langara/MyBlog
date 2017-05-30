36f82e0ecac9ccbf651a4d6997dd66c00ff62b85
Marek Langiewicz <marek.langiewicz@gmail.com>
2017-05-30 17:50:57 +0200

    I cancellation is cooperative 3 "withTimeout"

--- src/main/kotlin/pl/mareklangiewicz/myblog/coroutines/A_Coroutines_Intro.kt
+++ src/main/kotlin/pl/mareklangiewicz/myblog/coroutines/A_Coroutines_Intro.kt
@@ -235,6 +235,31 @@ class A_Coroutines_Intro {
     }
 
     /**
+     * Cancellation with timeout
+     *
+     * This example shows `withTimeout` function
+     * @sample pl.mareklangiewicz.myblog.coroutines.A_Coroutines_Intro.I_cancellationIsCooperative_3_timeout
+     */
+    @Test fun I_cancellationIsCooperative_3_withTimeout() = sample {
+        "main: start.".p
+        launch(CommonPool) {
+            withTimeout(100) { // change to 10000 to see the difference
+                var nextPrintTime = 0L
+                while (true) { // computation loop
+                    yield() // this will throw CancellationException on timeout
+                    val currentTime = System.currentTimeMillis()
+                    if (currentTime >= nextPrintTime) {
+                        "I'm sleeping...".p
+                        nextPrintTime = currentTime + 500L
+                    }
+                }
+            }
+        }
+        delay(3000L) // delay a bit
+        "main: end.".p
+    }
+
+    /**
      * Under the hood test
      *
      * @sample pl.mareklangiewicz.myblog.coroutines.A_Coroutines_Intro.J_underTheHood_1
